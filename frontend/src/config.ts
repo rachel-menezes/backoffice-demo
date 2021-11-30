b963f25ef9cadbb51e9957fde9c86fabd2a39539
Loris Wintjens <loris.wintjens@gmail.com>
2021-11-29 19:16:46 -0500

    env vars

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 import { Config } from './types';
-export const baseApiUri = process.env.NODE_ENV === 'development' ? "http://127.0.0.1:5000/api/v1" : "https://python-api-formal.herokuapp.com/api/v1";
+export const baseApiUri = process.env.REACT_APP_API_URL || '';
 
 export const config: Config = {
     name: process.env.REACT_APP_COMPANY_NAME || '',
