04cd838a48a19dbd7ffdb82defc92ace29aaf0f6
Mokhtar <mokhtar@joinformal.com>
2023-12-18 22:38:54 -0500

    remove data

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,10 +1,10 @@
 import { Config } from './types';
-export const baseApiUri = process.env.REACT_APP_API_URL || 'https://backoffice-ecgz.onrender.com/api/v1';
+export const baseApiUri = process.env.REACT_APP_API_URL || '';
 
 export const config: Config = {
-    name: process.env.REACT_APP_COMPANY_NAME || 'Rippling',
-    logoUrl: process.env.REACT_APP_COMPANY_LOGO || 'https://imagedelivery.net/YV7tiPr1pNu8eWcEhMhZuA/fd5fa43c-9fd0-4067-8b05-a6adc4d12300/public',
-    primaryColor: process.env.REACT_APP_PRIMARY_COLOR || '#fdb71c',
-    secondaryColor: process.env.REACT_APP_SECONDARY_COLOR || '#fff',
+    name: process.env.REACT_APP_COMPANY_NAME || '',
+    logoUrl: process.env.REACT_APP_COMPANY_LOGO || '',
+    primaryColor: process.env.REACT_APP_PRIMARY_COLOR || '',
+    secondaryColor: process.env.REACT_APP_SECONDARY_COLOR || '',
     thirdColor: process.env.REACT_APP_THIRD_COLOR || '',
 };
