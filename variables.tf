bf27d3bdea5d942da458a4fb88045b36e54916e4
Loris Wintjens <loris@joinformal.com>
2024-01-28 00:48:27 +0100

    add apprunner

--- variables.tf
+++ variables.tf
@@ -6,3 +6,7 @@ variable "database_name" {}
 variable "database_password" {}
 variable "database_url" {}
 variable "database_user" {}
+
+variable "public_subnets" {
+  default = ["172.0.16.0/20", "172.0.48.0/20", "172.0.80.0/20"]
+}
