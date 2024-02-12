6825413831c520b1f589f74eade16e3c140b0c07
rachel-menezes <menezesrachel31@gmail.com>
2024-02-12 11:58:33 -0800

    Update api.py
    Removing DB call

--- backend/python-api/api/api.py
+++ backend/python-api/api/api.py
@@ -16,64 +16,64 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-host = os.getenv('DATABASE_URL')
-dbName = os.getenv('DATABASE_NAME')
-user = os.getenv('DATABASE_USER')
-password = os.getenv('DATABASE_PASSWORD')
+# host = os.getenv('DATABASE_URL')
+# dbName = os.getenv('DATABASE_NAME')
+# user = os.getenv('DATABASE_USER')
+# password = os.getenv('DATABASE_PASSWORD')
 
-users_json = os.getenv('USERS')
-if users_json:
-    app.logger.info('Loading users...')
-    users = json.loads(users_json)
-else:
-    app.logger.info('No USERS set as environment variable...')
-    users = [] 
+# users_json = os.getenv('USERS')
+# if users_json:
+#     app.logger.info('Loading users...')
+#     users = json.loads(users_json)
+# else:
+#     app.logger.info('No USERS set as environment variable...')
+#     users = [] 
 
 
 @app.route('/', methods=['GET'])
 def home():
     return "<p>Hello World </p>"
 
-@app.route('/healthcheck', methods=['GET'])
-def healthcheck():
-    return "<p>ok</p>"
+# @app.route('/healthcheck', methods=['GET'])
+# def healthcheck():
+#     return "<p>ok</p>"
 
 
-@app.route('/api/v1/fetch-all', methods=["GET"])
-def fetch():
-    if 'endUserID' in request.args:
-        endUserID = int(request.args['endUserID'])
-    else:
-        return "Error: No end user id field provided. Please specify an endUserID."
-    try:
-        cursor_factory = CommenterCursorFactory()
-        conn = psycopg2.connect(
-            host=host,
-            database=dbName,
-            user=user,
-            password=password,
-            cursor_factory=cursor_factory)
-        cursor = conn.cursor()
-        cursor.execute("select * from pii;", "mokhtar@maytana.com")
-        return jsonify(cursor.fetchall())
-    except Exception as e:
-        print(e)
-        return "Error: an error occured. Please try again."
+# @app.route('/api/v1/fetch-all', methods=["GET"])
+# def fetch():
+#     if 'endUserID' in request.args:
+#         endUserID = int(request.args['endUserID'])
+#     else:
+#         return "Error: No end user id field provided. Please specify an endUserID."
+#     try:
+#         cursor_factory = CommenterCursorFactory()
+#         conn = psycopg2.connect(
+#             host=host,
+#             database=dbName,
+#             user=user,
+#             password=password,
+#             cursor_factory=cursor_factory)
+#         cursor = conn.cursor()
+#         cursor.execute("select * from pii;", "mokhtar@maytana.com")
+#         return jsonify(cursor.fetchall())
+#     except Exception as e:
+#         print(e)
+#         return "Error: an error occured. Please try again."
 
 
-@app.route('/api/v1/sign-in', methods=['POST'])
-def login():
-    email = request.get_json().get('email')
-    password = request.get_json().get('password')
-    app.logger.info("Trying to log user %s with password %s into the app", email, password)
-    if not email or not password:
-        return "Error: No username or password field provided. Please specify both."
-    try:
-        app.logger.info(users)
-        app.logger.info(type(users))
-        res = [u for u in users if u['email'] ==
-               email and u['password'] == password]
+# @app.route('/api/v1/sign-in', methods=['POST'])
+# def login():
+#     email = request.get_json().get('email')
+#     password = request.get_json().get('password')
+#     app.logger.info("Trying to log user %s with password %s into the app", email, password)
+#     if not email or not password:
+#         return "Error: No username or password field provided. Please specify both."
+#     try:
+#         app.logger.info(users)
+#         app.logger.info(type(users))
+#         res = [u for u in users if u['email'] ==
+#                email and u['password'] == password]
 
-        return jsonify(res[0])
-    except:
-        return "Error: an error occured. Please try again."
+#         return jsonify(res[0])
+#     except:
+#         return "Error: an error occured. Please try again."
