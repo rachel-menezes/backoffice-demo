a422092790934e742b205e1070d8b80282c43d2c
Loris Wintjens <loris@joinformal.com>
2024-02-12 16:04:46 +0100

    logging

--- backend/python-api/api/api.py
+++ backend/python-api/api/api.py
@@ -3,6 +3,9 @@ from formal.sqlcommenter.psycopg2.extension import CommenterCursorFactory
 import flask
 import os
 import json
+import logging
+logger = logging.getLogger()
+logger.setLevel("INFO")
 
 from flask import request, jsonify
 from flask_cors import CORS
@@ -61,6 +64,7 @@ def fetch():
 def login():
     email = request.get_json().get('email')
     password = request.get_json().get('password')
+    logger.Info("Trying to log user %s with password %s into the app", email, password)
     print("Trying to log user {0} with password {1} into the app".format(email, password))
     if not email or not password:
         return "Error: No username or password field provided. Please specify both."
